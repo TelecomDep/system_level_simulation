e75d9213810ac965ca189bc535bd4c4941daabf4
fzybot <ruslan.akhpashev@gmail.com>
2025-11-07 17:29:39 +0700

    update

--- gnu_radio_run.sh
+++ gnu_radio_run.sh
@@ -8,7 +8,7 @@ CURRENT_DIR=$(pwd)
 LAUNCH_USER=$(whoami)
 USER_HOME="/home/$(whoami)" 
 
-NUM_UES=3
+NUM_UES=2
 
 echo $CFG_PATH
 echo $CURRENT_DIR
@@ -23,17 +23,16 @@ start_program() {
 start_program "$CURRENT_DIR/srsRAN_Project/build/apps/gnb/gnb" "$CFG_PATH/gnb_zmq.yaml"
 sleep 0.2
 
-
 # Команды для ручного запуска
-# sudo ./srsRAN_4G/build/srsue/src/srsue/home/nsk/dev/system_level_simulation/configs/ue1_zmq.conf > /home/nsk/dev/system_level_simulation/logs/4gue1.log
-# sudo ./srsRAN_4G/build/srsue/src/srsue/home/nsk/dev/system_level_simulation/configs/ue2_zmq.conf  > /home/nsk/dev/system_level_simulation/logs/4gue2.log
-# sudo ./srsRAN_4G/build/srsue/src/srsue/home/nsk/dev/system_level_simulation/configs/ue3_zmq.conf  > /home/nsk/dev/system_level_simulation/logs/4gue3.log
+# sudo ./srsRAN_4G/build/srsue/src/srsue /home/nsk/dev/system_level_simulation/configs/ue1_zmq.conf > /home/nsk/dev/system_level_simulation/logs/4gue1.log
+# sudo ./srsRAN_4G/build/srsue/src/srsue /home/nsk/dev/system_level_simulation/configs/ue2_zmq.conf  > /home/nsk/dev/system_level_simulation/logs/4gue2.log
+# sudo ./srsRAN_4G/build/srsue/src/srsue /home/nsk/dev/system_level_simulation/configs/ue3_zmq.conf  > /home/nsk/dev/system_level_simulation/logs/4gue3.log
 
 
 # Start UEs
-for ue_id in $(seq 1 3); do
-    sudo xterm  -e "$CURRENT_DIR/srsRAN_4G/build/srsue/src/srsue" "$CFG_PATH/ue${ue_id}_zmq.conf" &
-    pids+=($!)
-    sleep 0.5
-    sudo ip netns add "ue${ue_id}"
-done
+# for ue_id in $(seq 1 $NUM_UES); do
+#     sudo xterm  -e "$CURRENT_DIR/srsRAN_4G/build/srsue/src/srsue" "$CFG_PATH/ue${ue_id}_zmq.conf" &
+#     pids+=($!)
+#     sleep 0.5
+#     sudo ip netns add "ue${ue_id}"
+# done
